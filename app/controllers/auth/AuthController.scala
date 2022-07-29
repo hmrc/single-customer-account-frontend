2c645c9bbdf994166c8b0bb6b0c8c512d05626e1
yangwong9664 <20066247+yangwong9664@users.noreply.github.com>
2022-07-29 13:43:00 +0100

    fix signout

--- app/controllers/auth/AuthController.scala
+++ app/controllers/auth/AuthController.scala
@@ -40,7 +40,7 @@ class AuthController @Inject()(
         .clear(request.userId)
         .map {
           _ =>
-            Redirect(config.signOutUrl, Map("continue" -> Seq(config.exitSurveyUrl)))
+            Redirect(config.signOutUrl, Map("continue" -> Seq(config.exitSurveyUrl))).withNewSession
       }
   }
 
@@ -50,7 +50,7 @@ class AuthController @Inject()(
       .clear(request.userId)
       .map {
         _ =>
-        Redirect(config.signOutUrl, Map("continue" -> Seq(routes.SignedOutController.onPageLoad.url)))
+        Redirect(config.signOutUrl, Map("continue" -> Seq(routes.SignedOutController.onPageLoad.url))).withNewSession
       }
   }
 }
