1c81e44ba7970af18461f541ad1123c37a78668a
yangwong9664 <20066247+yangwong9664@users.noreply.github.com>
2022-09-27 16:24:56 +0100

    code improvements

--- app/controllers/YourTaxesAndBenefitsController.scala
+++ app/controllers/YourTaxesAndBenefitsController.scala
@@ -33,7 +33,7 @@ class YourTaxesAndBenefitsController @Inject()(
                                )(implicit frontendAppConfig: FrontendAppConfig) extends FrontendBaseController with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = (authenticate andThen getUserDetails) { implicit request =>
-    val name = request.ifData.details.name.fold("null"){ name => s"${name.firstForename.getOrElse("null")} ${name.surname.getOrElse("null")}"}
+    val name = request.ifData.details.name.fold(""){ name => s"${name.firstForename.getOrElse("")} ${name.surname.getOrElse("")}"}
     val nino = request.authenticatedRequest.nino
     Ok(view(name, nino))
   }
