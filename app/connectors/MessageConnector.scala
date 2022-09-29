77e0ff3a534b369dcd219a1a9d889df5a7898023
Kritika Rai <111003013+kritikarai6@users.noreply.github.com>
2022-09-29 16:59:36 +0100

    Message screen changes

--- app/connectors/MessageConnector.scala
+++ app/connectors/MessageConnector.scala
@@ -44,7 +44,7 @@ class MessageConnector @Inject()(
   def getMessages(requestHeader: RequestHeader): Future[HtmlPartial] = {
     implicit val hc: HeaderCarrier = headerCarrierForPartialsConverter.fromRequestWithEncryptedCookie(requestHeader)
 
-    http.GET[HtmlPartial]("http://localhost:9060/messages") map {
+    http.GET[HtmlPartial]("http://localhost:9060/messages/VEEvZHR4cE85S3BWcUx0Z0ErNnFReHFXbS9uNlIzWGJLMC9BTHo3YWVySU03d3lLeGNxbUxNQmFwdXFMUEJ2ODNNRmpwZ3BoZy9qZHRuSkNSNHBiYjI5R1RHMVAxeDJwOTlXSWFEZGNhQXlWSDk0MU9HWlJXVy9vdWZNSVlyR1lQRWNrTEVXSEZjTS9FVktIVUhsa2VBNHI2a1k2bHRIeW1kZTZtYy9waEs4bXF4TzhZYkUvdWcrNFUxSDRjeElKYjFVRFFGVG5mczN5eVJaakw0T0FRWkI0a0Y4SlRrQ0RSN0lKNUpPdVNqQjUwbUQ5dnQ0elBpQjQxNXNQc0JvZHNicEhPRk9lbUdRaUhEMjFRczYvQm5YUG9OUmQydzJ1MC9yTDVWS250M1hxZHVPZGxuaUcrWmRRbjMwT3JQcjhvM2ZKZWtTcjhoVlNDTDZFMTF1ZlhPUXFjNEh1OGZrWExSMTJYcDBUdGxVdTdlY3BtM1hXUGY2QThML1djSjhhd1E5dVFRcVhJKy81dXVqTWxJTndLYTh2YVZhWkxkdWp3Rk1Zb0taZjJuRnROS2hSTjFJTWFucm85S1hJanFtMFZZQnh5dUZXUUJRWkFHcGU1dFNnbXc9PQ==") map {
       case partial: HtmlPartial.Success => partial
       case partial: HtmlPartial.Failure =>
         logger.error(s"Failed to load partial, partial info: $partial")
@@ -57,6 +57,7 @@ class MessageConnector @Inject()(
       }
     }
   }
+
 
 
 }
