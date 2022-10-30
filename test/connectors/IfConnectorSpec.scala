e0a39fe1e4e5a2f2d757d6d36564222f747089c4
Kritika Rai <111003013+kritikarai6@users.noreply.github.com>
2022-10-30 19:24:38 +0000

    test cases added for code coverage

--- test/connectors/IfConnectorSpec.scala
+++ test/connectors/IfConnectorSpec.scala
@@ -16,15 +16,104 @@
 
 package connectors
 
-import fixtures.SpecBase
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.client.WireMock.{ok, urlEqualTo}
+import controllers.actions.AuthActionImpl
+import fixtures.RetrievalOps.Ops
+import fixtures.{SpecBase, WireMockHelper}
+import models.integrationframework.{IFContactDetail, IFContactDetails, IfAddress, IfAddressList, IfDesignatoryDetails, IfDetails, IfName, IfNameList}
+import play.api.test.Helpers.running
+import uk.gov.hmrc.auth.core.{AuthConnector, ConfidenceLevel, CredentialStrength, Enrolments}
+import uk.gov.hmrc.domain.Nino
 
-class IfConnectorSpec extends SpecBase{
+import java.time.LocalDate
+import scala.concurrent.Future
+
+class IfConnectorSpec extends SpecBase with WireMockHelper {
+
+  lazy val mockAuthConnector: AuthConnector = mock[AuthConnector]
+  lazy val authAction = new AuthActionImpl(mockAuthConnector, frontendAppConfigInstance, bodyParserInstance)
+  lazy val connector = injector.instanceOf[IFConnector]
+  server.start()
+
+
+  applicationBuilder.configure(
+    "microservice.services.integration-framework.port" -> server.port(),
+    "metrics.enabled" -> false,
+    "auditing.enabled" -> false,
+    "auditing.traceRequests" -> false
+  )
+    .build()
 
   val nino = "AA999999A"
+
   val designatoryDetailsFields: String =
     "details(marriageStatusType),nameList(name(nameSequenceNumber,nameType,titleType," +
       "requestedName,nameStartDate,nameEndDate,firstForename,secondForename,surname))," +
       "addressList(address(addressSequenceNumber,countryCode,addressType,addressStartDate," +
       "addressEndDate,addressLine1,addressLine2,addressLine3,addressLine4,addressLine5," +
       "addressPostcode))"
+
+  val contactDetailsFields: String = "contactDetails(code,type,detail)"
+
+  "calling If connector getDesignatoryDetails" must {
+    "return IfDesignatoryDetails when response can be parsed" in {
+      val url = s"/individuals/details/NINO/${nino}?fields=$designatoryDetailsFields"
+      running(app) {
+
+        val expectedObj = Some(IfDesignatoryDetails(
+          details = IfDetails(Option(4)),
+          nameList = IfNameList(Seq(
+            IfName(
+              nameSequenceNumber = Some(1),
+              nameType = Some(1),
+              titleType = Some(1),
+              firstForename = Some("John"),
+              secondForename = Some("X"),
+              surname = Some("Johnson"),
+              requestedName = Some("Johnny")
+            ))),
+          addressList = IfAddressList(Seq(IfAddress(
+            addressSequenceNumber = Some(2),
+            countryCode = Some(1),
+            addressType = Some(1),
+            addressLine1 = Some("1 Home Road"),
+            addressLine2 = Some("Home Town"),
+            addressLine3 = Some("Home Region"),
+            addressLine4 = Some("Home Area"),
+            addressLine5 = Some("Home Shire"),
+            addressPostcode = Some("XX77 6YY")
+          )))
+        ))
+
+        server.stubFor(
+          WireMock.get(urlEqualTo(url))
+            .willReturn(ok(expectedObj.toString()))
+        )
+
+        connector.getDesignatoryDetails(Some(Nino(nino))).futureValue mustEqual expectedObj
+      }
+    }
+  }
+
+
+/* "calling If connector getContactsDetails" must {
+    "return IfContactDetails when response can be parsed" in {
+      val url = s"/individuals/details/contact/nino/${nino}?fields=$contactDetailsFields"
+      running(app) {
+        val contactDetails = Some(IFContactDetails(
+          contactDetails = Some(Seq(
+            IFContactDetail(code = 9, contactType = "MOBILE TELEPHONE", detail = "07123 987654"),
+            IFContactDetail(code = 7, contactType = "DAYTIME TELEPHONE", detail = "07123 987654"),
+            IFContactDetail(code = 8, contactType = "EVENING TELEPHONE", detail = "07123 987654"),
+            IFContactDetail(code = 11, contactType = "PRIMARY E-MAIL", detail = "fred.blogs@hotmail.com")
+          ))))
+      server.stubFor(
+        WireMock.get(urlEqualTo(url))
+          .willReturn(ok(contactDetails.toString))
+      )
+      connector.getContactDetails(Some(Nino(nino))).futureValue mustEqual contactDetails
+    }
+  }
+}*/
 }
--- test/repositories/SessionRepositorySpec.scala
+++ test/repositories/SessionRepositorySpec.scala
@@ -16,23 +16,99 @@
 
 package repositories
 
-import fixtures.{SpecBase, WireMockHelper}
-import play.api.http.Status.OK
-import play.api.test.Helpers.status
+import config.FrontendAppConfig
+import fixtures.SpecBase
+import models.session.Session
+import org.mockito.Mockito.when
+import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
 
-import scala.concurrent.Future
+import java.time.{Clock, Instant, LocalDateTime, ZoneId}
 
-class SessionRepositorySpec extends SpecBase with WireMockHelper{
 
-  lazy val repository = injector.instanceOf[SessionRepository]
 
-  /*"Session repository.keepAlive" must {
+class SessionRepositorySpec extends SpecBase with DefaultPlayMongoRepositorySupport[Session]{
 
-    "return 200" in {
+ private val instant = Instant.now()
+  private val stubClock: Clock = Clock.fixed(instant, ZoneId.systemDefault)
+  private val session = Session("id", LocalDateTime.now(), Some("abc"))
 
-      val result: Future[Boolean] = repository.keepAlive(id = "id")
+  private val mockAppConfig = mock[FrontendAppConfig]
+  when(mockAppConfig.cacheTtl) thenReturn 1
 
-      result mustBe Future[Boolean]
+  lazy val repository = new SessionRepository(
+    mongoComponent = mongoComponent,
+    appConfig = mockAppConfig,
+    clock = stubClock
+  )
+
+  " calling set method" must {
+
+    "must set the last updated time on the supplied session to `now`, and save them" in {
+
+      val setResult = repository.set(session).futureValue
+
+      setResult mustEqual true
     }
-  }*/
+  }
+
+ /* "calling get" must {
+
+      "must return the session based on id" in {
+
+        insert(session).futureValue
+        val expectedResult = session copy ("id", LocalDateTime.now(), Some("abc"))
+        val result = repository.get("id").futureValue
+
+        result mustEqual expectedResult
+      }
+    }*/
+
+    " calling get when there is no record for this id" must {
+
+      "must return None" in {
+
+        repository.get("id that does not exist").futureValue must not be defined
+      }
+    }
+
+
+  "calling clear" must {
+
+    "must remove a record" in {
+
+      insert(session).futureValue
+
+      val result = repository.clear(session.id).futureValue
+
+      result mustEqual true
+      repository.get(session.id).futureValue must not be defined
+    }
+
+    "must return true when there is no record to remove" in {
+      val result = repository.clear("id that does not exist").futureValue
+
+      result mustEqual true
+    }
+  }
+
+  " calling keepAlive when there is record" must {
+
+
+      "must return true" in {
+
+        insert(session).futureValue
+
+        val result = repository.keepAlive(session.id).futureValue
+        result mustEqual true
+      }
+    }
+
+    "calling keep alive when there is no record for this id" must {
+
+      "must return true" in {
+
+        repository.keepAlive("id that does not exist").futureValue mustEqual true
+      }
+    }
+
 }
