826a8eeda02e58af88aee34de260bbedf6e9aa08
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2023-03-13 16:15:10 +0000

    update library version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.68.0",
     "uk.gov.hmrc"       %% "domain"                         % s"8.0.0-play-28",
 //    "uk.gov.hmrc"       %% "sca-wrapper"                    % "1.0.0-SNAPSHOT"
-    "uk.gov.hmrc"       %% "sca-wrapper"                    % "1.0.9"
+    "uk.gov.hmrc"       % "sca-wrapper_2.13"                    % "1.0.9"
   )
 
   val test = Seq(
