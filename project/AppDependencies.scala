d7c161303b955f1a694cd2453c5c634dfaad579e
Cem Yeniceri <97523669+cyeniceri@users.noreply.github.com>
2023-03-22 16:22:40 +0000

    Refactoring (#52)
    code refactored, dependencies upgraded

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,23 +8,20 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "6.8.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.14.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.1.0",
 //    "uk.gov.hmrc"       %% "sca-wrapper"                    % "1.0.0-SNAPSHOT"
     "uk.gov.hmrc"       %% "sca-wrapper"                    % "1.0.16"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% s"bootstrap-test-play-28" % "7.14.0",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28" % "7.14.0",
     "org.scalatest"           %% "scalatest"               % "3.2.15",
-    "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
-    "org.scalatestplus"       %% "mockito-3-4"             % "3.2.10.0",
+    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.15.0",
+    "org.scalatestplus"       %% "mockito-4-6"             % "3.2.15.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "5.1.0",
     "org.pegdown"             %  "pegdown"                 % "1.6.0",
     "org.jsoup"               %  "jsoup"                   % "1.15.4",
     "com.typesafe.play"       %% "play-test"               % PlayVersion.current,
     "org.mockito"             %% "mockito-scala"           % "1.17.12",
-    "org.scalacheck"          %% "scalacheck"              % "1.17.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28" % "1.1.0",
     "com.vladsch.flexmark"    %  "flexmark-all"            % "0.62.2",
     "com.github.tomakehurst"  %  "wiremock-standalone"     % "2.27.2"
 
