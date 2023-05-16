9739729ce689dbd36d50db057d6c1a6637b85853
adiliqbal02 <118364388+adiliqbal02@users.noreply.github.com>
2023-05-16 15:59:18 +0100

    FS-1086: Attempt to fix CapabilityDetailsControllerSpec & CapabilityConnectorSpec

--- test/controllers/CapabilityDetailsControllerSpec.scala
+++ test/controllers/CapabilityDetailsControllerSpec.scala
@@ -16,38 +16,68 @@
 
 package controllers
 
+import connectors.CapabilityConnector
 import fixtures.SpecBase
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import models.integrationframework.IfCapabilityDetails
+import org.mockito.ArgumentMatchers._
+import org.scalatest.BeforeAndAfter
+import play.api.Application
+import play.api.inject.guice.{GuiceApplicationBuilder, GuiceableModule}
+import play.api.inject.bind
 import play.api.test.FakeRequest
-import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout}
+import uk.gov.hmrc.http.HeaderCarrier
+//import org.mockito.Mockito.{verify, when}
+import org.mockito.Mockito._
+import org.scalatest.concurrent.ScalaFutures.whenReady
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import play.api.http.Status.OK
+import play.api.libs.json.Json
+import play.api.mvc.Results._
+import play.api.mvc.{MessagesControllerComponents, Result}
+import play.api.test.Helpers.{contentAsJson, contentAsString, defaultAwaitTimeout, status}
 import services.CapabilityService
+import uk.gov.hmrc.auth.core.Nino
+import uk.gov.hmrc.domain
 import views.html.CapabilityDetailsView
 
+import scala.concurrent.Future
 
-class CapabilityDetailsControllerSpec extends SpecBase {
-  lazy val capabilityService = injector.instanceOf[CapabilityService]
+class CapabilityDetailsControllerSpec extends SpecBase with BeforeAndAfter {
 
-  lazy val capabilityDetailsView: CapabilityDetailsView = injector.instanceOf[CapabilityDetailsView]
-//lazy val controller: CapabilityDetailsController = new CapabilityDetailsController(messagesControllerComponents,capabilityService, capabilityDetailsView)
-  lazy val controller: CapabilityDetailsController = injector.instanceOf[CapabilityDetailsController]
+  lazy val capabilityService = mock[CapabilityService]
+  lazy val capabilityDetailsView: CapabilityDetailsView = mock[CapabilityDetailsView]
+  lazy val controller: CapabilityDetailsController = new CapabilityDetailsController(messagesControllerComponents,capabilityService, capabilityDetailsView)
+  lazy val mockCapabilitiesConnector: CapabilityConnector = mock[CapabilityConnector]
+
+  val nino = domain.Nino("GG012345C")
   val expectedDate = "9 April 2023"
   val expectedDescription = "Your tax code has changed"
   val expectedUrl = "www.tax.service.gov.uk/check-income-tax/tax-code-change/tax-code-comparison"
 
   "CapabilityDetailsController" must {
-    "Return the capabilityDetails page" in {
+    "Return the capabilityDetails page successfully with correct data" in {
+      val capabilityDetails = IfCapabilityDetails(
+        nino = Nino(true, Some("GG012345C")),
+        date = "9 April 2023",
+        descriptionContent = "Your tax code has changed",
+        url = "www.tax.service.gov.uk/check-income-tax/tax-code-change/tax-code-comparison"
+      )
 
-      whenReady(controller.getCapabilitiesData(fakeRequest)) { result =>
-        result.header.status shouldBe 200
-      }
-    }
+      when(capabilityService.getCapabilityDetails(any())(any())).thenReturn(Future.successful(capabilityDetails))
 
-    "Return correct data on capabilityDetails page" in {
       val result = controller.getCapabilitiesData(fakeRequest)
-      contentAsString(result) must include(expectedDate)
-      contentAsString(result) must include(expectedDescription)
-      contentAsString(result) must include(expectedUrl)
-
+      whenReady(result) { _ =>
+        status(result) mustBe OK
+        contentAsJson(result) mustBe Json.obj(
+          "nino" -> Json.obj(
+            "hasNino" -> true,
+            "nino" -> "GG012345C"
+          ),
+          "date" -> "9 April 2023",
+          "descriptionContent" -> "Your tax code has changed",
+          "url" -> "www.tax.service.gov.uk/check-income-tax/tax-code-change/tax-code-comparison"
+        )
+      }
     }
   }
 }
