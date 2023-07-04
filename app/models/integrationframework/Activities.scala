4a29857f54a48f01d02a44f130210d800e8483e5
adiliqbal02 <118364388+adiliqbal02@users.noreply.github.com>
2023-07-04 10:55:24 +0100

    FS-1223: Updated connector, controller & view to incorporate changes to Activities in capabilities MS & stubs

--- app/models/integrationframework/Activities.scala
+++ app/models/integrationframework/Activities.scala
@@ -20,7 +20,7 @@ import play.api.libs.json.{Format, Json}
 
 case class Activities(
   taxCalc: Seq[CapabilityDetails],
-  taxCode: Seq[CapabilityDetails],
+  taxCode: Option[CapabilityDetails],
   childBenefit: Seq[CapabilityDetails],
   payeIncome: Seq[CapabilityDetails]
 )
