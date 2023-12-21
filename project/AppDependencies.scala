edac9d6ab2fa33f7aaa1cda4e7d750a8f15a6759
owen.gilfellon <owen.gilfellon@opencastsoftware.com>
2023-12-21 15:22:40 +0000

    SO-300 | Testing

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,16 @@ import sbt._
 
 object AppDependencies {
 
+  val playVersion =       "play-30"
+  val bootstrapVersion =  "8.1.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"                 %% s"bootstrap-frontend-play-30"      % "8.1.0",
-    "uk.gov.hmrc"       %% "sca-wrapper-play-30"            % "1.1.0-SNAPSHOT"
+    "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"   % bootstrapVersion,
+    "uk.gov.hmrc"       %% "sca-wrapper-play-30"                % "1.1.0-SNAPSHOT"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"                   %% s"bootstrap-test-play-30"        % "8.1.0",
-
+    "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion"       % bootstrapVersion
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
