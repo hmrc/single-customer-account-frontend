4a740a9696741e84ae36de44c93ea56fc31e828c
owen.gilfellon <owen.gilfellon@opencastsoftware.com>
2023-12-29 10:43:25 +0000

    SO-300 | Addressed PR Bot suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,6 @@ object AppDependencies {
   val bootstrapVersion =  "8.1.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"   % bootstrapVersion,
     "uk.gov.hmrc"       %% "sca-wrapper-play-30"                % "1.1.0"
   )
 
