17126e027070c2ea6dade42fbd8dd28c1b4064be
grantrobson <grant.robson@digital.hmrc.gov.uk>
2025-06-30 13:44:43 +0100

    10595 Update versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,10 +3,10 @@ import sbt.*
 object AppDependencies {
 
   val playVersion = "play-30"
-  val bootstrapVersion = "9.12.0"
+  val bootstrapVersion = "9.13.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% s"sca-wrapper-$playVersion" % "2.10.0"
+    "uk.gov.hmrc" %% s"sca-wrapper-$playVersion" % "2.14.0"
   )
 
   val test = Seq(
