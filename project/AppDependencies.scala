124ffb787257e63db6044ed47b971a53b4d3d499
MrSleeth <5717589+MrSleeth@users.noreply.github.com>
2025-09-22 16:27:54 +0100

    Update bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   val playVersion = "play-30"
-  val bootstrapVersion = "9.19.0"
+  val bootstrapVersion = "10.1.0"
 
   val compile = Seq(
     "uk.gov.hmrc" %% s"sca-wrapper-$playVersion" % "4.3.0"
