5da1597ae90e9b26d8b7dcb781e856c9d69313c7
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2026-04-23 15:05:06 +0100

    DDCNL-11864: Updated libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 object AppDependencies {
 
   val playVersion = "play-30"
-  val bootstrapVersion = "10.1.0"
+  val bootstrapVersion = "10.7.0"
 
-  val compile = Seq(
-    "uk.gov.hmrc" %% s"sca-wrapper-$playVersion" % "4.3.0"
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% s"sca-wrapper-$playVersion" % "5.0.0"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootstrapVersion
   ).map(_ % Test)
 
