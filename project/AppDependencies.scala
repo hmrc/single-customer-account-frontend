a9c61dfefe3eeaca749a1a749f23286958d03945
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2026-06-22 16:31:29 +0100

    Update dependencies versions in AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,13 @@ import sbt.*
 object AppDependencies {
 
   val playVersion = "play-30"
-  val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% s"sca-wrapper-$playVersion" % "5.0.0"
+    "uk.gov.hmrc" %% s"sca-wrapper-$playVersion" % "5.3.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% s"bootstrap-test-$playVersion" % bootstrapVersion
+    "uk.gov.hmrc" %% s"sca-wrapper-test-$playVersion" % "5.3.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
